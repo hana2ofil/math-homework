68959a258d3884e9263ea6674771f708060ace63
hana2ofil <hana2ofil@gmx.com>
2025-03-03 14:00:16 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class math_homework{
+  public static void main(String[] args){
+    Random r=new Random();
+    int n1=r.nextInt(10);
+    int n2=r.nextInt(10);
+    System.out.println("The sum of "+n1+" and "+n2+ " is: "+(n1+n2));
+  }
+}
